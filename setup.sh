4ab576ee5c4847f066589603d97513fae6f9e96a
Đỗ Quang Duy <103756160+qdw106311@users.noreply.github.com>
2025-08-07 15:47:59 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,6 @@
+home='/data/data/com.termux/files/'
+printf '\n\033[1;92m Tiến Hành Cài Dữ Liệu\n\n'
+curl -s https://raw.githubusercontent.com/qdw106311/g/main/te.php -o $home/usr/bin/dh
+chmod 777 $home/usr/bin/dh
+
+printf '\n\033[1;92m Nhập \033[1;93mdh \033[1;92mĐể Vào Tool <33\n\n'
